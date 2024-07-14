358c5f07c290dc06062e51e1b5cb21a732c05631
ZhaochengNiu <790173661@qq.com>
2024-07-14 22:55:44 +0800

    update

--- plot/energy_consumption_in_each_slot.py
+++ plot/energy_consumption_in_each_slot.py
@@ -4,10 +4,10 @@ import seaborn as sns
 
 sns.set_theme(context='notebook', style='whitegrid', palette='deep', font='sans-serif', font_scale=1.0, color_codes=True, rc=None)
 
-Local = np.loadtxt('./result/LocalTotalUtilityInEachSlotFilePath10.cache')
-Nearest = np.loadtxt('./result/NearestTotalUtilityInEachSlotFilePath10.cache')
-Random = np.loadtxt('./result/RandomTotalUtilityInEachSlotFilePath10.cache')
-Game = np.loadtxt('./result/GameTotalUtilityInEachSlotFilePath10.cache')
+Local = np.loadtxt('../result/LocalEnergyConsumptionInEachSlotFilePath10.cache')
+Nearest = np.loadtxt('../result/NearestEnergyConsumptionInEachSlotFilePath10.cache')
+Random = np.loadtxt('../result/RandomEnergyConsumptionInEachSlotFilePath10.cache')
+Proposed = np.loadtxt('./result/ProposedTotalEnergyConsumptionInEachSlotFilePath10.cache')
 
 # 生成数据
 x = np.arange(0, 1000, 5)  # 横坐标数据为从0到1000之间，步长为1的等差数组
@@ -15,19 +15,19 @@ x = np.arange(0, 1000, 5)  # 横坐标数据为从0到1000之间，步长为1的
 Local_temp = []
 Nearest_temp = []
 Random_temp = []
-Game_temp = []
+Proposed_temp = []
 
 for i in range(0, 1000, 5):
     Local_temp.append(Local[i])
     Nearest_temp.append(Nearest[i])
     Random_temp.append(Random[i])
-    Game_temp.append(Game[i])
+    Proposed_temp.append(Proposed[i])
 
 # 生成图形
 Local, = plt.plot(x, Local_temp, color='#bb3f3f', alpha=0.7, linestyle='-', linewidth=2)
 Nearest, = plt.plot(x, Nearest_temp, color='#fcb001', alpha=0.7, linestyle='-', linewidth=2)
 Random, = plt.plot(x, Random_temp, color='#016795', alpha=0.7, linestyle='-', linewidth=2)
-Game, = plt.plot(x, Game_temp, color='#ad03de', alpha=0.7, linestyle='-', linewidth=2)
+Proposed, = plt.plot(x, Proposed_temp, color='#ad03de', alpha=0.7, linestyle='-', linewidth=2)
 
 # alpha：不透明度（值为“0”时为透明状态，默认为“1”）
 # linestype：线条类型
@@ -38,10 +38,10 @@ plt.xlim(0, 1000)
 
 
 plt.xlabel('Time slot (t)')
-plt.ylabel('Total utility in the system (MB)')
+plt.ylabel('Total energy consumption in the system (MB)')
 # 设置legend
-plt.legend(handles=[Local, Nearest, Random, Game], labels=['Local', 'Nearest', 'Random', 'Game'], loc='best')
+plt.legend(handles=[Local, Nearest, Random, Proposed], labels=['Local', 'Nearest', 'Random', 'Proposed'], loc='best')
 
-plt.savefig('./TotalUtilityInEachSlot10.pdf', format='pdf')
+plt.savefig('./EnergyConsumptionInEachSlot10.pdf', format='pdf')
 # 显示图形
 plt.show()
--- plot/total_cost.py
+++ plot/total_cost.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_theme(context='notebook', style='whitegrid', palette='deep', font='sans-serif', font_scale=1.0, color_codes=True, rc=None)
 
-Local = np.loadtxt('./result/LocalTotalCostInEachSlotFilePath10.cache')
-Nearest = np.loadtxt('./result/NearestTotalCostInEachSlotFilePath10.cache')
-Random = np.loadtxt('./result/RandomTotalCostInEachSlotFilePath10.cache')
-Proposed = np.loadtxt('./result/ProposedTotalCostInEachSlotFilePath10.cache')
+Local = np.loadtxt('../result/LocalTotalCostInEachSlotFilePath10.cache')
+Nearest = np.loadtxt('../result/NearestTotalCostInEachSlotFilePath10.cache')
+Random = np.loadtxt('../result/RandomTotalCostInEachSlotFilePath10.cache')
+Proposed = np.loadtxt('../result/ProposedTotalCostInEachSlotFilePath10.cache')
 
 Local_utility = sum(Local)
 Nearest_utility = sum(Nearest)
